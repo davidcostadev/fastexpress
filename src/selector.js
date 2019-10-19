82dc5911b2e24389ac091a9e67205be39430782b
David Costa <davidcostadev@gmail.com>
2019-10-19 19:38:42 -0300

    Remove complexity

--- src/selector.js
+++ src/selector.js
@@ -1,29 +1,33 @@
-const R = require('ramda');
+const { has, prop, toPairs, not, last, isNil } = require('ramda');
 
 const convertIt = (definition, key, value) =>
-  R.has('convert', definition) ? definition.convert(value, key) : value;
+  has('convert', definition) ? definition.convert(value, key) : value;
+
+// eslint-disable-next-line consistent-return
+const defaultIt = definition => {
+  if (has('default', definition)) {
+    return definition.default;
+  }
+};
+
+const filterQueries = (definition, key, query, callback) => {
+  if (has(key, query)) {
+    return callback(definition, key, prop(key, query));
+  }
+  return defaultIt(definition);
+};
+
+const checkValidation = (definition, key, value) => {
+  if (definition.validation(value)) {
+    return convertIt(definition, key, value);
+  }
+  return defaultIt(definition);
+};
 
 const selector = (definitions, query = {}) =>
-  R.toPairs(definitions).reduce((acc, [key, definition]) => {
-    let newValue;
-    if (R.has(key, query)) {
-      const value = R.prop(key, query);
-
-      if (definition.validation(value)) {
-        newValue = convertIt(definition, key, value);
-      } else if (R.has('default', definition)) {
-        newValue = definition.default;
-      }
-    } else if (R.has('default', definition)) {
-      newValue = definition.default;
-    }
-
-    return newValue
-      ? {
-          ...acc,
-          [key]: newValue,
-        }
-      : acc;
-  }, {});
+  toPairs(definitions)
+    .map(([key, definition]) => [key, filterQueries(definition, key, query, checkValidation)])
+    .filter(entry => not(isNil(last(entry))))
+    .reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {});
 
 module.exports = selector;
